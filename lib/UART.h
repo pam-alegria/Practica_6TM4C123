8b46416b5f2d2d54c32e732b72c3ad8ab7e72778
pam-alegria <palegria11@alumnos.uaq.mx>
2022-12-15 09:27:14 -0600

    Practica 3

--- lib/UART.h
+++ lib/UART.h
@@ -1,11 +1,13 @@
 #ifndef UART_UART_H_
 #define UART_UART_H_
 
-extern void Configurar_UART0(void);
+extern void Configurar_UART2(void);
 extern char readChar(void);
 extern void printChar(char c);
 extern void printString(char* string);
-extern char * readString(char delimitador);
+extern int readString(char delimitador, char *string);
+extern void invertirString(char *string, int i);
+extern void agregarNumeros(char *string, char *num, int i);
 
 
 #endif /* UART_UART_H_ */
