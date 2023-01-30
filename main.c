5f8a8f3c704dd355b64c3082df42e868df90cda9
pam-alegria <palegria11@alumnos.uaq.mx>
2023-01-29 23:19:19 -0600

    Peor es nada

--- main.c
+++ main.c
@@ -2,13 +2,42 @@
 
 int main(void)
 {
+    Configurar_PLL(_40MHZ);  //Confiuracion de velocidad de reloj
+    Configura_Reg_ADC0();
+    Configurar_UART0();
+    Configura_Reg_PWM1();
     initTimer0();
-    initADC0();
 
     // Main loop
     while(1)
     {
         uint32_t adcValue1 = readADC0();
         uint32_t adcValue2 = readADC0();
+
+        while ((ADC0->RIS & 0x1) == 0)
+        {
+        }
+
+        // Read the ADC result for channel 0
+        uint32_t adcResult0 = ADC0->SSFIFO0 & 0xFFF;
+
+        // Clear the ADC interrupt flag for channel 0
+        ADC0->ISC = 0x1;
+
+        // Start the ADC conversion for channel 1
+        ADC0->PSSI |= 0x2;
+
+        while ((ADC0->RIS & 0x2) == 0)
+        {
+        }
+
+        // Read the ADC result for channel 1
+        uint32_t adcResult1 = ADC0->SSFIFO1 & 0xFFF;
+        // Clear the ADC interrupt flag for channel 1
+        ADC0->ISC = 0x2;
+
+        // Process the ADC results for both channels here
+
+
     }
 }
